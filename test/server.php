c61aed1c2f33b0665e89c096d407f2d3694a1d4e
Nikos M <workingclasscode@gmail.com>
2024-09-08 15:56:17 +0300

    multiple autonomous tico apps in subfolders test

--- test/server.php
+++ test/server.php
@@ -6,12 +6,28 @@
 
 $__DIR__ = dirname(__FILE__);
 
-$uri = /*urldecode(*/parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)/*)*/;
+$uri = rtrim(/*urldecode(*/parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)/*)*/, '/');
 if (!strlen($uri)) $uri = '/';
 
-//if ('/index.php' == substr($uri, -10)) $uri = substr($uri, 0, -9); // remove '/index.php' from uri
+if ('/index.php' == $uri)
+{
+    $uri = '/';
+}
+elseif ('/subfolder/index.php' == $uri)
+{
+    $uri = '/subfolder';
+}
 
-if ($uri !== '/' && $uri !== '/subfolder/' && file_exists($__DIR__ . '/' . $uri)) return false; // existing file, serve as-is
+if ($uri != '/' && $uri != '/subfolder' && file_exists($__DIR__ . $uri))
+{
+    return false; // existing file, serve as-is
+}
 
-if (0 === strpos($uri, '/subfolder/')) include($__DIR__ . '/subfolder/index.php'); // dispatch to subfolder app
-else include($__DIR__ . '/index.php'); // dispatch to main app
+if ('/subfolder' == $uri || 0 === strpos($uri, '/subfolder/'))
+{
+    include($__DIR__ . '/subfolder/index.php'); // dispatch to subfolder app
+}
+else
+{
+    include($__DIR__ . '/index.php'); // dispatch to main app
+}
